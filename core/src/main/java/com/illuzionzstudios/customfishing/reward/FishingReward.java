8c051e85f5f984f74bd15dda7dbbaedbfeb47ace
IlluzionzDev <jaminstratford@gmail.com>
2020-07-12 15:05:25 +1200

    Add materials to each option

--- core/src/main/java/com/illuzionzstudios/customfishing/reward/FishingReward.java
+++ core/src/main/java/com/illuzionzstudios/customfishing/reward/FishingReward.java
@@ -9,6 +9,7 @@
  */
 package com.illuzionzstudios.customfishing.reward;
 
+import com.illuzionzstudios.mist.compatibility.XMaterial;
 import com.illuzionzstudios.mist.compatibility.XSound;
 import com.illuzionzstudios.mist.config.Configurable;
 import com.illuzionzstudios.mist.config.locale.Message;
@@ -32,91 +33,91 @@ public class FishingReward {
     /**
      * Name of the reward
      */
-    @Configurable(description = "rewards.config.descriptions.name")
+    @Configurable(description = "rewards.config.descriptions.name", material = XMaterial.NAME_TAG)
     private String name;
 
     /**
      * Commands to execute
      */
-    @Configurable(description = "rewards.config.descriptions.commands")
+    @Configurable(description = "rewards.config.descriptions.commands", material = XMaterial.COMMAND_BLOCK)
     private List<String> commands = new ArrayList<>();
 
     /**
      * Custom items to give
      */
-    @Configurable(description = "rewards.config.descriptions.items")
+    @Configurable(description = "rewards.config.descriptions.items", material = XMaterial.APPLE)
     private List<ItemStack> items = new ArrayList<>();
 
     /**
      * Messages to send
      */
-    @Configurable(description = "rewards.config.descriptions.messages")
+    @Configurable(description = "rewards.config.descriptions.messages", material = XMaterial.OAK_SIGN)
     private List<String> messages = new ArrayList<>();
 
     /**
      * Broadcast messages to send
      */
-    @Configurable(description = "rewards.config.descriptions.broadcasts")
+    @Configurable(description = "rewards.config.descriptions.broadcasts", material = XMaterial.ACACIA_SIGN)
     private List<String> broadcasts = new ArrayList<>();
 
     /**
      * Title message
      */
-    @Configurable(description = "rewards.config.descriptions.title")
+    @Configurable(description = "rewards.config.descriptions.title", material = XMaterial.PAPER)
     private Message title;
 
     /**
      * Subtitle message
      */
-    @Configurable(description = "rewards.config.descriptions.subtitle")
+    @Configurable(description = "rewards.config.descriptions.subtitle", material = XMaterial.PAPER)
     private Message subtitle;
 
     /**
      * Chance to find this reward
      */
-    @Configurable(description = "rewards.config.descriptions.chance")
+    @Configurable(description = "rewards.config.descriptions.chance", material = XMaterial.SPAWNER)
     private double chance;
 
     /**
      * If default vanilla rewards are enabled
      */
-    @Configurable(description = "rewards.config.descriptions.vanillaRewards")
+    @Configurable(description = "rewards.config.descriptions.vanillaRewards", material = XMaterial.FISHING_ROD)
     private boolean vanillaRewards;
 
     /**
      * Experience to give the player
      */
-    @Configurable(description = "rewards.config.descriptions.experience")
+    @Configurable(description = "rewards.config.descriptions.experience", material = XMaterial.EXPERIENCE_BOTTLE)
     private int experience;
 
     /**
      * Sound to play
      */
-    @Configurable(description = "rewards.config.descriptions.sound")
+    @Configurable(description = "rewards.config.descriptions.sound", material = XMaterial.NOTE_BLOCK)
     private XSound sound;
 
     /**
      * Required permission
      */
-    @Configurable(description = "rewards.config.descriptions.permission")
+    @Configurable(description = "rewards.config.descriptions.permission", material = XMaterial.BARRIER)
     private String permission;
 
     /**
      * Worlds the reward can be found in
      */
-    @Configurable(description = "rewards.config.descriptions.worlds")
+    @Configurable(description = "rewards.config.descriptions.worlds", material = XMaterial.GRASS_BLOCK)
     private List<String> worlds = new ArrayList<>();
 
     /**
      * Region the reward can be found in
      */
-    @Configurable(description = "rewards.config.descriptions.regions")
+    @Configurable(description = "rewards.config.descriptions.regions", material = XMaterial.DIRT)
     private List<String> regions = new ArrayList<>();
 
     /**
      * Region the reward can't be found in
      */
-    @Configurable(description = "rewards.config.descriptions.blockedRegions")
+    @Configurable(description = "rewards.config.descriptions.blockedRegions", material = XMaterial.COARSE_DIRT)
     private List<String> blockedRegions = new ArrayList<>();
 
     /**
